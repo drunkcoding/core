62ef800123d060ee84ffeec5efdca419f984ed6a
future-xy <fuyao3860@gmail.com>
2022-12-21 15:18:10 +0000

    add frequency batcher

---
+++ build.sh
@@ -0,0 +1,8 @@
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=`pwd`/install \
+-DTRITON_ENABLE_ENSEMBLE=ON \
+-DTRITON_THIRD_PARTY_REPO_TAG=r22.08 \
+-DTRITON_COMMON_REPO_TAG=swap-engine \
+-DTRITON_CORE_HEADERS_ONLY=OFF ..
+make install -j
